d56ac891562c677203c66ef03bcd459870688a28
Aankirz <sahuankit453@gmail.com>
2022-07-27 07:12:23 +0530

    add

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const port=8000;
 
 //use express router
 app.use('/',require('./routes'));//by default it will look for index.js 
+app.set('view engine','ejs');
+app.set('views','./views');
 
 app.listen(port,function(err){
 if(err){
